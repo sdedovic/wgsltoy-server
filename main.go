6ef726c38407fc3706f003d19694a2cf377e247a
sdedovic <stevan@dedovic.com>
2024-09-29 20:47:24 -0500

    health check return json

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 )
 
+type RegisterUser struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Captcha  string `json:"Capture"`
+}
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(HealthResponse{"ok"})
 	})
 
+	fmt.Println("Starting server on 0.0.0.0:8080")
+
 	http.ListenAndServe(":8080", nil)
+
 }
